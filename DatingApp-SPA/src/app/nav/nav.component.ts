b078cd6476e25edcfe8508916fa792666d19893b
Ruben Macedo <ruben.macedo@it-objects.pt>
2019-01-23 17:52:30 +0000

    Section 6 lesson 57

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -10,7 +10,7 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
@@ -24,7 +24,6 @@ export class NavComponent implements OnInit {
   }
 
   loggedIn() {
-
    return this.authService.loggedin();
   }
 
