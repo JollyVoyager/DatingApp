6d804c424f22e17c2e88cca93206d881cd7a4e42
Ruben Macedo <ruben.macedo@it-objects.pt>
2019-02-05 11:14:59 +0000

    End of Section 15

--- DatingApp.API/Models/Photo.cs
+++ DatingApp.API/Models/Photo.cs
@@ -12,7 +12,10 @@ namespace DatingApp.API.Models
         public bool IsMain { get; set; }
         public string PublicId { get; set;}
         public User User { get; set; }
-
         public int UserId { get; set; }
+
+        
+
+
     }
 }
